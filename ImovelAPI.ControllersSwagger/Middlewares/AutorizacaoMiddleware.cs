48799f1ea57aa5c68e4de808bf70269da70c9441
little-junior <rampagedolol@outlook.com>
2024-02-08 20:15:36 -0300

    feat: implement another approach for AutorizacaoMiddleware

--- ImovelAPI.ControllersSwagger/Middlewares/AutorizacaoMiddleware.cs
+++ ImovelAPI.ControllersSwagger/Middlewares/AutorizacaoMiddleware.cs
@@ -15,20 +15,37 @@
             {
                 const string usuarioEsperado = "Admin";
 
-                if (context.Request.Headers.ContainsKey("UsuarioLogado"))
+                if (!context.Request.Headers.ContainsKey("UsuarioLogado"))
                 {
-                    if (context.Request.Headers.TryGetValue("UsuarioLogado", out var usuario))
-                    {
-                        if (usuario == usuarioEsperado)
-                        {
-                            await _next.Invoke(context);
-                            return;
-                        }
-                    }
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsJsonAsync("The header 'UsuarioLogado' is required");
+                    return;
                 }
 
-                context.Response.StatusCode = 401;
-            } catch (Exception ex)
+                if (!context.Request.Headers.TryGetValue("UsuarioLogado", out var usuario))
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsJsonAsync("The value of 'UsuarioLogado' couldn't be read");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(usuario))
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsJsonAsync("The value of 'UsuarioLogado' can't be null");
+                    return;
+                }
+
+                if (usuario != usuarioEsperado)
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsJsonAsync("Access denied");
+                    return;
+                }
+
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
             {
                 context.Response.StatusCode = 500;
                 await context.Response.WriteAsJsonAsync($"Authorization Error: {ex.Message} - Try Again Later");
