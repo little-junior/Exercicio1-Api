1ab94dccbf37eb59a404feeafd209537d4f79ef8
little-junior <rampagedolol@outlook.com>
2024-02-08 11:43:41 -0300

    feat: add JsonRequired attribute to properties

--- ImovelAPI.Domain/DTOs/ImovelDTO.cs
+++ ImovelAPI.Domain/DTOs/ImovelDTO.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ImovelAPI.Domain.DTOs
 {
     public class ImovelDTO
     {
+        [JsonRequired]
         public double Area { get; set; }
+        [JsonRequired]
         public string Tipo { get; set; }
     }
 }
