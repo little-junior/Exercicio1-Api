017a467add29b1ec6c16e2d94de436633b67a54b
little-junior <rampagedolol@outlook.com>
2024-02-09 19:33:16 -0300

    feat: add required attributes to properties

--- ImovelAPI.Domain/DTOs/ImovelDTO.cs
+++ ImovelAPI.Domain/DTOs/ImovelDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -9,9 +10,11 @@ namespace ImovelAPI.Domain.DTOs
 {
     public class ImovelDTO
     {
-        [JsonRequired]
+        [Required(ErrorMessage = "O campo 'area' é obrigatorio")]
         public double Area { get; set; }
-        [JsonRequired]
+
+        [Required(ErrorMessage = "O campo 'tipo' é obrigatorio")]
+        [MaxLength(30, ErrorMessage = "O campo 'tipo' deve ter no máximo 30 caracteres")]
         public string Tipo { get; set; }
     }
 }
