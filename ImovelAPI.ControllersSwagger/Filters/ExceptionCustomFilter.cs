db2be0b4f3b7d4c63c9adcd8ad772d8f75aba367
little-junior <rampagedolol@outlook.com>
2024-02-09 19:33:28 -0300

    feat: add filters

---
+++ ImovelAPI.ControllersSwagger/Filters/ExceptionCustomFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ImovelAPI.ControllersSwagger.Filters
+{
+    public class ExceptionCustomFilter : IAsyncExceptionFilter
+    {
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            context.Result = new ObjectResult("Error - Try Again Later ")
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+
+            await Task.CompletedTask;
+        }
+    }
+}
